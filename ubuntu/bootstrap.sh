2226704414bc29e9a3b76153446b08eefa1fdbee
ashon <ashon8813@gmail.com>
2015-03-27 00:00:39 +0900

    update simple bootstrap script

---
+++ ubuntu/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# update apt
+sudo apt-get update
+
+# install git, vim, nodejs, nodejs-legacy, npm
+sudo apt-get -y install git vim nodejs nodejs-legacy npm
+
+# install pylint
+sudo apt-get -y install pylint
+
