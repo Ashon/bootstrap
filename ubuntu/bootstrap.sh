db8f4fcfdba85b1f4c72eddf88abde8e65676c81
ashon <ashon8813@gmail.com>
2015-03-27 00:05:01 +0900

    update script

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -6,6 +6,8 @@ sudo apt-get update
 # install git, vim, nodejs, nodejs-legacy, npm
 sudo apt-get -y install git vim nodejs nodejs-legacy npm
 
-# install pylint
-sudo apt-get -y install pylint
+# install pylint pip
+sudo apt-get -y install pylint python-pip
 
+# install nmap tcpdump iotop iftop
+sudo apt-get -y install nmap tcpdump iotop iftop
