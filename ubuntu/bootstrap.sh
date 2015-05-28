e30f101e875ad6532d71f1b5556d2bdaf848f415
ashon <ashon8813@gmail.com>
2015-05-28 20:50:46 +0900

    update bootstrap

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -12,5 +12,8 @@ sudo apt-get -y install pylint python-pip
 # install nmap tcpdump iotop iftop
 sudo apt-get -y install nmap tcpdump iotop iftop
 
+# install gnome-do
+sudo apt-get -y install gnome-do
+
 # install bash-git-prompt
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
