b11db899856feceef02690f593b4883fb7e7301d
juwon.lee <juwon.lee@cdnetworks.com>
2016-02-11 17:07:32 +0900

    update dots

--- dots/import_dots.sh
+++ dots/import_dots.sh
@@ -1,2 +1,2 @@
 cp -a ~/.bashrc ~/.vimrc ~/.gitconfig ./
-cp -a ~/.bash-git-prompt/themes/Default.bgptheme .bash-git-prompt/themes/
+cp -a ~/.bash-git-prompt/themes/Jwl.bgptheme .bash-git-prompt/themes/
